e1730acb55c6147787b210e765748b1d575a97c4
swxk19 <s.wxk19@gmail.com>
2023-05-27 18:08:29 +0800

    Fix help message

--- globals.py
+++ globals.py
@@ -9,8 +9,8 @@ command_list = """
 data_file_name = "data.json"
 
 HELP_TEXT = """Commands:\n
-\start: :-)
-\\newBook: deletes everything and adds new people
-\end: finish adding people
-\log: view specifically what each person owes
-\pool: view summary of what each person contributes to pool"""
+/start: :-)
+/newBook: deletes everything and adds new people
+/end: finish adding people
+/log: view specifically what each person owes
+/pool: view summary of what each person contributes to pool"""
